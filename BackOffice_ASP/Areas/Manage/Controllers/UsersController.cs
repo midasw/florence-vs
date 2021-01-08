339b2c3412a4d330dfd7ffab494f8746a2f3a3d8
midasw <midas.wouters@gmail.com>
2021-01-08 06:07:14 +0100

    Added user manager controller + view

---
+++ BackOffice_ASP/Areas/Manage/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackOffice_ASP.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    [Authorize(Roles = "Super,Visibility,Admin")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
