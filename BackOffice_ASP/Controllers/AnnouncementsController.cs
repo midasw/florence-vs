16f7d4d067ec88111efa53e9f670a77191b0e9fb
midasw <midas.wouters@gmail.com>
2021-01-08 06:15:45 +0100

    Created AnnouncementsController + views

---
+++ BackOffice_ASP/Controllers/AnnouncementsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BackOffice_ASP.Data;
+using BackOffice_ASP.Models;
+
+namespace BackOffice_ASP.Controllers
+{
+    public class AnnouncementsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AnnouncementsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Announcements
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Announcements.ToListAsync());
+        }
+
+        // GET: announcements/view/5
+        public async Task<IActionResult> View(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var announcement = await _context.Announcements
+                .Include(a => a.Author)
+                .Include(a => a.Editor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (announcement == null)
+            {
+                return NotFound();
+            }
+
+            return View(announcement);
+        }
+
+        // GET: Announcements/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Announcements/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DateCreated,DateEdited,Subject,Body,BodyParsed,IsPublished,IsEdited")] Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(announcement);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(announcement);
+        }
+
+        // GET: Announcements/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var announcement = await _context.Announcements.FindAsync(id);
+            if (announcement == null)
+            {
+                return NotFound();
+            }
+            return View(announcement);
+        }
+
+        // POST: Announcements/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DateCreated,DateEdited,Subject,Body,BodyParsed,IsPublished,IsEdited")] Announcement announcement)
+        {
+            if (id != announcement.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(announcement);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnnouncementExists(announcement.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(announcement);
+        }
+
+        // GET: Announcements/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var announcement = await _context.Announcements
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (announcement == null)
+            {
+                return NotFound();
+            }
+
+            return View(announcement);
+        }
+
+        // POST: Announcements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var announcement = await _context.Announcements.FindAsync(id);
+            _context.Announcements.Remove(announcement);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnnouncementExists(int id)
+        {
+            return _context.Announcements.Any(e => e.Id == id);
+        }
+    }
+}
