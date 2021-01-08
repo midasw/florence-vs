2d4a7d32ad3f5757ddc6964a8430c08ad24aeab1
midasw <midas.wouters@gmail.com>
2021-01-08 06:18:13 +0100

    Added model class for API authentication

---
+++ BackOffice_ASP/Entities/ApiUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackOffice_ASP.Entities
+{
+    public class ApiUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
