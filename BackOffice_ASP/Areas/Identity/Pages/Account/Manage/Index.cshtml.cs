b49b51d24fc13548c5f4cea1e0323a364919a1c2
midasw <midas.wouters@gmail.com>
2021-01-08 06:04:16 +0100

    Updated identity razor pages

--- BackOffice_ASP/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ BackOffice_ASP/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using BackOffice_ASP.Data;
+using BackOffice_ASP.Helpers;
 using BackOffice_ASP.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,19 +23,22 @@ namespace BackOffice_ASP.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
         public IndexModel(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
-            ApplicationDbContext context)
+            ApplicationDbContext context,
+            IWebHostEnvironment webHostEnvironment)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        //public string Username { get; set; }
-        public SelectList Countries { get; set; }
+        public string Avatar { get; set; }
+        public string AvatarUrl { get; set; }
 
         [TempData]
         public string StatusMessage { get; set; }
@@ -39,54 +48,15 @@ namespace BackOffice_ASP.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Display(Name = "First name")]
-            public string FirstName { get; set; }
-
-            [Display(Name = "Last name")]
-            public string LastName { get; set; }
-
-            [Display(Name = "Street address")]
-            public string StreetAddress { get; set; }
-
-            [Display(Name = "City")]
-            public string CityName { get; set; }
-
-            [Display(Name = "ZIP")]
-            public string PostalCode { get; set; }
-
-            [Display(Name = "State code")]
-            public string StateCode { get; set; }
-
-            [Display(Name = "Country")]
-            public string CountryCode { get; set; }
-
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
+            public IFormFile Image { get; set; }
         }
 
-        private async Task LoadAsync(ApplicationUser user)
+        private void Load(ApplicationUser user)
         {
-            //var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            Avatar = user.Avatar;
+            AvatarUrl = user.AvatarUrl;
 
-            //Username = userName;
-            var countries = _context.Countries.OrderBy(c => c.Name).ToList();
-            Countries = new SelectList(countries, "Code", "Name");
-
-            Input = new InputModel
-            {
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-
-                StreetAddress = user.StreetAddress,
-                CityName = user.CityName,
-                PostalCode = user.PostalCode,
-                StateCode = user.StateCode,
-                CountryCode = user.Country?.Code,
-
-                PhoneNumber = phoneNumber
-            };
+            Input = new InputModel();
         }
 
         public async Task<IActionResult> OnGetAsync()
@@ -97,11 +67,11 @@ namespace BackOffice_ASP.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            await LoadAsync(user);
+            Load(user);
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(bool? delete = false)
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
@@ -111,44 +81,81 @@ namespace BackOffice_ASP.Areas.Identity.Pages.Account.Manage
 
             if (!ModelState.IsValid)
             {
-                await LoadAsync(user);
+                Load(user);
                 return Page();
             }
 
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
+            var uploads = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+
+            if (delete == true && !string.IsNullOrEmpty(user.Avatar))
             {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
+                var origFilePath = Path.Combine(uploads, user.Avatar);
+                if (System.IO.File.Exists(origFilePath))
                 {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
+                    System.IO.File.Delete(origFilePath);
+                }
+
+                user.Avatar = null;
+
+                var result = await _userManager.UpdateAsync(user);
+                if (!result.Succeeded)
+                {
+                    StatusMessage = "Unexpected error when trying to delete avatar.";
                     return RedirectToPage();
                 }
             }
-
-            //user.FirstName = Input.FirstName;
-            //user.LastName = Input.LastName;
-
-            user.StreetAddress = Input.StreetAddress;
-            user.CityName = Input.CityName;
-            user.PostalCode = Input.PostalCode;
-            user.StateCode = Input.StateCode;
-
-            var country = _context.Countries.FirstOrDefault(c => c.Code == Input.CountryCode);
-            if (country != null)
+            else if (Input.Image != null && Input.Image.Length > 0)
             {
-                user.Country = country;
+                using (var memoryStream = new MemoryStream())
+                {
+                    await Input.Image.CopyToAsync(memoryStream);
+
+                    try
+                    {
+                        var img = Image.FromStream(memoryStream);
+
+                        const int width = 300;
+                        const int height = 300;
+
+                        if (img.Width > width || img.Height > height)
+                        {
+                            img = img.ResizeImage(width, height);
+                        }
+
+                        var uniqueFileName = $@"avatar-{Guid.NewGuid().ToString("N")}.jpg";
+                        var filePath = Path.Combine(uploads, uniqueFileName);
+
+                        img.Save(filePath, ImageFormat.Jpeg);
+
+                        if (!string.IsNullOrEmpty(user.Avatar))
+                        {
+                            var origFilePath = Path.Combine(uploads, user.Avatar);
+                            if (System.IO.File.Exists(origFilePath))
+                            {
+                                System.IO.File.Delete(origFilePath);
+                            }
+                        }
+
+                        user.Avatar = uniqueFileName;
+
+                        var result = await _userManager.UpdateAsync(user);
+                        if (!result.Succeeded)
+                        {
+                            StatusMessage = "Unexpected error when trying to update avatar.";
+                            return RedirectToPage();
+                        }
+                    }
+                    catch
+                    {
+                        StatusMessage = "Invalid file type";
+                        return RedirectToPage();
+                    }
+                }
+
+                StatusMessage = "Successfully updated avatar";
+                await _signInManager.RefreshSignInAsync(user);
             }
 
-            var result = await _userManager.UpdateAsync(user);
-            if (!result.Succeeded)
-            {
-                StatusMessage = "Unexpected error when trying to update profile.";
-                return RedirectToPage();
-            }
-
-            await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your profile has been updated";
             return RedirectToPage();
         }
     }
--- BackOffice_ASP/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ BackOffice_ASP/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -8,6 +8,8 @@ namespace BackOffice_ASP.Areas.Identity.Pages.Account.Manage
 {
     public static class ManageNavPages
     {
+        public static string Profile => "Profile";
+
         public static string Index => "Index";
 
         public static string Email => "Email";
@@ -23,6 +25,8 @@ namespace BackOffice_ASP.Areas.Identity.Pages.Account.Manage
         public static string PersonalData => "PersonalData";
 
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
+
+        public static string ProfileNavClass(ViewContext viewContext) => PageNavClass(viewContext, Profile);
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
