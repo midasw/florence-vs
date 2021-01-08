61175711d7633efec37ec2f30009354c41516f7f
midasw <midas.wouters@gmail.com>
2021-01-08 06:24:21 +0100

    Added manage viewmodels

---
+++ BackOffice_ASP/Areas/Manage/ViewModels/EditAnnouncementViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackOffice_ASP.Areas.Manage.ViewModels
+{
+    public class EditAnnouncementViewModel
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
