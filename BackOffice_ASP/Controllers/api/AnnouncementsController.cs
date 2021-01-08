cf371a50d5bd042af08ef316d14bc8d12711af87
midasw <midas.wouters@gmail.com>
2021-01-08 06:18:30 +0100

    Added basic API

---
+++ BackOffice_ASP/Controllers/api/AnnouncementsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackOffice_ASP.Data;
+using BackOffice_ASP.Models;
+using BackOffice_ASP.ViewModels.api;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace BackOffice_ASP.Controllers.api
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/announcements")]
+    [ApiController]
+    public class AnnouncementsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AnnouncementsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Announcements
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AnnouncementViewModel>>> GetAnnouncements()
+        {
+            return await _context.Announcements
+                .Include(a => a.Author.Country)
+                .Include(a => a.Editor.Country)
+                .Select(a => new AnnouncementViewModel(a))
+                .ToListAsync();
+        }
+
+        // GET: api/Announcements/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AnnouncementViewModel>> GetAnnouncement(int id)
+        {
+            var announcement = await _context.Announcements
+                .Include(a => a.Author)
+                .Include(a => a.Editor)
+                .Where(a => a.Id == id)
+                .Select(a => new AnnouncementViewModel(a))
+                .FirstOrDefaultAsync();
+
+            if (announcement == null)
+            {
+                return NotFound();
+            }
+
+            return announcement;
+        }
+
+        // PUT: api/Announcements/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAnnouncement(int id, Announcement announcement)
+        {
+            if (id != announcement.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(announcement).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnnouncementExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Announcements
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Announcement>> PostAnnouncement(Announcement announcement)
+        {
+            _context.Announcements.Add(announcement);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAnnouncement", new { id = announcement.Id }, announcement);
+        }
+
+        // DELETE: api/Announcements/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnnouncement(int id)
+        {
+            var announcement = await _context.Announcements.FindAsync(id);
+            if (announcement == null)
+            {
+                return NotFound();
+            }
+
+            _context.Announcements.Remove(announcement);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AnnouncementExists(int id)
+        {
+            return _context.Announcements.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BackOffice_ASP/Controllers/api/UserController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackOffice_ASP.Data;
+using BackOffice_ASP.Models;
+using BackOffice_ASP.ViewModels.api;
+using BackOffice_ASP.Entities;
+using Microsoft.AspNetCore.Identity;
+using BackOffice_ASP.Helpers;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace BackOffice_ASP.Controllers.api
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly AppSettings _appSettings;
+
+        public UserController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IOptions<AppSettings> appSettings
+            )
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _appSettings = appSettings.Value;
+        }
+
+
+        // GET: api/Users
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUserViewModel>>> GetUsers()
+        {
+            return await _context.Users
+                .Include(u => u.Country)
+                .Select(u => new ApplicationUserViewModel(u))
+                .ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUserViewModel>> GetUser(string id)
+        {
+            var user = await _context.Users
+                .Include(u => u.Country)
+                .Include(u => u.UserRoles)
+                .ThenInclude(userRole => userRole.Role)
+                .Where(u => u.Id == id)
+                .Select(u => new ApplicationUserViewModel(u))
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<object> Authenticate([FromBody] ApiUser apiUser)
+        {
+            var applicationUser = await _userManager.FindByEmailAsync(apiUser.Email);
+
+            if (applicationUser != null)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(
+                applicationUser.UserName, apiUser.Password, false, false);
+
+                if (signInResult.Succeeded)
+                {
+                    apiUser.Token = _appSettings.GenerateJwtToken(applicationUser).ToString();
+                    return apiUser;
+                }
+            }
+
+            return BadRequest(new { message = "Email or password is incorrect" });
+        }
+    }
+}
